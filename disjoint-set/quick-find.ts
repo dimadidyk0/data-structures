78e2499b057131c87954413ca088d38582d04220
dimadidyk0 <dimadidyk0@gmail.com>
2024-08-04 20:31:43 +0200

    feat: add quick find

---
+++ disjoint-set/quick-find.ts
@@ -0,0 +1,35 @@
+class UnionFind {
+  private root: number[];
+  constructor(length: number) {
+    this.root = Array.from({ length }, (_, i) => i);
+  } 
+
+  public find = (a: number) => {
+    return this.root[a];
+  }
+
+  public union(a: number, b: number) {
+    const rootA = this.find(a);
+    const rootB = this.find(b);
+    if (rootA !== rootB) {
+      for (let i = 0; i < this.root.length; i++) {
+        if (this.root[i] === rootB) {
+          this.root[i] = rootA;
+        }
+      }
+    }
+  }
+}
+
+const set = new UnionFind(10);
+
+set.union(0, 1);
+set.union(0, 2);
+set.union(0, 3);
+set.union(0, 4);
+set.union(0, 5);
+set.union(6, 7);
+set.union(9, 8);
+set.union(9, 0);
+
+console.log(set);
